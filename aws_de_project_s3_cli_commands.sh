3c9e94b575038051acb5f8bef8bb5ebfdcd41156
Nihar Dave <njdave1998@gmail.com>
2024-10-26 20:42:56 +0530

    commit

---
+++ aws_de_project_s3_cli_commands.sh
@@ -0,0 +1,14 @@
+# Copy all JSON data to same location:
+s3 cp . s3://de-project-data-raw-dev/youtube/raw_statistics_reference_data/ --recursive --exclude "*" --include "*.json"
+
+# Copy all data files to its own location i.e. its own folder:
+aws s3 cp CAvideos.csv s3://de-project-data-raw-dev/youtube/raw_statistics/region=ca/
+aws s3 cp DEvideos.csv s3://de-project-data-raw-dev/youtube/raw_statistics/region=de/
+aws s3 cp FRvideos.csv s3://de-project-data-raw-dev/youtube/raw_statistics/region=fr/
+aws s3 cp GBvideos.csv s3://de-project-data-raw-dev/youtube/raw_statistics/region=gb/
+aws s3 cp INvideos.csv s3://de-project-data-raw-dev/youtube/raw_statistics/region=in/
+aws s3 cp JPvideos.csv s3://de-project-data-raw-dev/youtube/raw_statistics/region=jp/
+aws s3 cp KRvideos.csv s3://de-project-data-raw-dev/youtube/raw_statistics/region=kr/
+aws s3 cp MXvideos.csv s3://de-project-data-raw-dev/youtube/raw_statistics/region=mx/
+aws s3 cp RUvideos.csv s3://de-project-data-raw-dev/youtube/raw_statistics/region=ru/
+aws s3 cp USvideos.csv s3://de-project-data-raw-dev/youtube/raw_statistics/region=us/
